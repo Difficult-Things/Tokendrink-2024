360780a48c9e571036fac0fa3b43a149893d3bca
MLGino <ginoalthof@live.com>
2024-03-12 23:12:45 +0100

    round and state update via mqtt

--- game-dashboard/src/app/page.tsx
+++ game-dashboard/src/app/page.tsx
@@ -1,11 +1,51 @@
 "use client";
-
+import React, {useEffect} from 'react'
+import mqtt from "mqtt"; // import namespace "mqtt"
 import { Control } from "@/components/control";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Stats } from "@/components/stats";
 
 export default function Home() {
+  const [client, setClient] = React.useState<any>(null);
+  const [pullenwandConnected, setPullenwandConnected] = React.useState("OFFLINE")
+  const [maingameConnected, setMaingameConnected] = React.useState("OFFLINE")
+
+  const mqttConnect = (host:any, mqttOption:any) => {
+    // setConnectStatus('Connecting');
+    setClient(mqtt.connect(host, mqttOption));
+  };
+
+  
+useEffect(() => {
+  if (client) {
+    console.log(client);
+    client.on('connect', () => {
+      console.log("Connected")
+      client.subscribe("gamevisuals/status")
+      client.subscribe("pullenwand/status")
+    });
+    client.on('error', (err : any) => {
+      console.error('Connection error: ', err);
+      client.end();
+    });
+    client.on('reconnect', () => {
+    });
+    client.on('message', (topic : string, message : any) => {
+      if(topic === "gamevisuals/status"){
+        setMaingameConnected(message.toString())
+      }
+      if(topic === "pullenwand/status"){
+        setPullenwandConnected(message.toString())
+      }
+    });
+  }
+}, [client]);
+
+useEffect(() => {
+  setClient(mqtt.connect("mqtt://127.0.0.1:8888"))
+}, [])
+  
   return (
     <main className="min-h-screen flex flex-col">
       <div className="flex-grow-0 flex-shrink basis-auto">
@@ -15,7 +55,7 @@ export default function Home() {
       <div className="flex-grow flex-shrink basis-auto">
         <div className="flex flex-row">
           <div className="flex-grow flex-shrink basis-0 mx-4 rounded-lg border">
-            <Control />
+            <Control client={client}/>
           </div>
           <div className="flex-grow flex-shrink basis-0 mx-4 rounded-lg border">
             <Stats />
@@ -24,6 +64,10 @@ export default function Home() {
       </div>
 
       <div className="flex-grow-0 flex-shrink basis-[40px] text-center">
+
+        <p>Maingame is {maingameConnected}</p>
+        <p>Pullenwand is {pullenwandConnected}</p>
+
         <Footer />
       </div>
     </main>
--- game-dashboard/src/components/control.tsx
+++ game-dashboard/src/components/control.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import { Button } from "./ui/button";
 import { Tabs, TabsList } from "./ui/tabs";
 import { TabsTrigger } from "@radix-ui/react-tabs";
+import * as Select from "@radix-ui/react-select";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import "./alertStyling.css";
 
-export function Control() {
+export function Control(props: any) {
+  const [round, setRound] = React.useState("1");
+  const [roundState, setRoundState] = React.useState("drinking");
+
   return (
     <div className="flex flex-col m-4 gap-4">
       <h2 className="text-lg font-bold">CONTROLS</h2>
@@ -18,9 +24,103 @@ export function Control() {
             <TabsTrigger value="5">5</TabsTrigger>
           </TabsList>
         </Tabs>
-        <Button variant={"destructive"} className="w-32">
-          Launch 🚀
-        </Button>
+        <div className="flex row">
+          <div className="m-5">
+            <Select.Root value={round} onValueChange={setRound}>
+              <Select.Trigger>
+                <Select.Value aria-label={round}>{round}</Select.Value>
+                <Select.Icon />
+              </Select.Trigger>
+              <Select.Portal>
+                <Select.Content position="popper" sideOffset={5} className="bg-slate-700">
+                  <Select.Viewport>
+                    <Select.Item value="1">
+                      <Select.ItemText>Roulette</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="2">
+                      <Select.ItemText>Slots</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="3">
+                      <Select.ItemText>Plinko</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="4">
+                      <Select.ItemText>Grijper</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="5">
+                      <Select.ItemText>Paardenrace</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </div>
+
+          <div className="m-5">
+            <Select.Root value={roundState} onValueChange={setRoundState}>
+              <Select.Trigger>
+                <Select.Value aria-label={roundState}>{roundState}</Select.Value>
+                <Select.Icon />
+              </Select.Trigger>
+              <Select.Portal>
+                <Select.Content position="popper" sideOffset={5} className="bg-slate-700">
+                  <Select.Viewport>
+                    <Select.Item value="drinking">
+                      <Select.ItemText>drinking</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="reveal">
+                      <Select.ItemText>reveal</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                    <Select.Item value="play">
+                      <Select.ItemText>play</Select.ItemText>
+                      <Select.ItemIndicator>…</Select.ItemIndicator>
+                    </Select.Item>
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </div>
+        </div>
+
+        <AlertDialog.Root>
+          <AlertDialog.Trigger asChild>
+            <button className="Button violet"> Launch 🚀</button>
+          </AlertDialog.Trigger>
+          <AlertDialog.Portal>
+            <AlertDialog.Overlay className="AlertDialogOverlay" />
+            <AlertDialog.Content className="AlertDialogContent">
+              <AlertDialog.Title className="AlertDialogTitle">Are you absolutely sure?</AlertDialog.Title>
+              <AlertDialog.Description className="AlertDialogDescription">This will update the game!!</AlertDialog.Description>
+              <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
+                <AlertDialog.Cancel asChild>
+                  <button className="Button mauve">Cancel</button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action asChild>
+                  <button
+                    className="Button red"
+                    onClick={() => {
+                      props.client.publish("gamevisuals/gamestate", JSON.stringify({round: parseInt(round), state: roundState, ranking: {
+                        red: 1,
+                        green: 2,
+                        orange: 3,
+                        blue: 4,
+                        purple: 5
+                      }}));
+                    }}
+                  >
+                    Yes, update game state
+                  </button>
+                </AlertDialog.Action>
+              </div>
+            </AlertDialog.Content>
+          </AlertDialog.Portal>
+        </AlertDialog.Root>
       </div>
     </div>
   );
